d105d07db26960a6e0bebf52f0eb8956ccfe5df6
Azula100 <zulchimegzula@gmail.com>
2026-06-16 23:20:47 +0800

    Update newsRoute.js

--- routes/newsRoute.js
+++ routes/newsRoute.js
@@ -1,9 +1,7 @@
-const express    = require('express')
-const router     = express.Router()
+const express = require('express')
+const router = express.Router()
 const fileupload = require('express-fileupload')
-const {
-  getAllNews, getNewsById, createNews, updateNews, deleteNews
-} = require('../controller/newsController')
+const {getAllNews, getNewsById, createNews, updateNews, deleteNews} = require('../controller/newsController')
 const { protect, authorize } = require('../middleware/auth')
 
 const upload = fileupload({
@@ -12,10 +10,9 @@ const upload = fileupload({
   limits: { fileSize: 10 * 1024 * 1024 }
 })
 
-router.get('/',     getAllNews)
-router.get('/:id',  getNewsById)
-router.post('/',    protect, authorize('user','editor','admin'), upload, createNews)
-router.put('/:id',  protect, upload, updateNews)
+router.get('/', getAllNews)
+router.get('/:id', getNewsById)
+router.post('/', protect, authorize('user','editor','admin'), upload, createNews)
+router.put('/:id', protect, upload, updateNews)
 router.delete('/:id', protect, deleteNews)
-
 module.exports = router
