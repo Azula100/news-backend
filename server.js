b8435d98bbf2ce0dea1a698bc1594b2ccdd2016d
Azula100 <zulchimegzula@gmail.com>
2026-06-16 23:19:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-const express      = require('express')
-const dotenv       = require('dotenv')
-const cors         = require('cors')
+const express = require('express')
+const dotenv = require('dotenv')
+const cors  = require('cors')
 const cookieParser = require('cookie-parser')
-const morgan       = require('morgan')
+const morgan = require('morgan')
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config({ path: './config/config.env' })
 }
-
 const connectDB = require('./config/db')
 connectDB()
-
 const app = express()
-
 app.use(cors({ origin: '*', credentials: true }))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
